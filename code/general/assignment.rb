eb84297950613fd07208aed69c06ecb712587de9
Charlie Somerville <charlie@charliesomerville.com>
2015-08-02 15:34:06 +1000

    return nil from assignment benchmarks

--- code/general/assignment.rb
+++ code/general/assignment.rb
@@ -2,6 +2,7 @@ require 'benchmark/ips'
 
 def slow
   a, b, c, d, e, f, g, h = 1, 2, 3, 4, 5, 6, 7, 8
+  nil
 end
 
 def fast
@@ -13,6 +14,7 @@ def fast
   f = 6
   g = 7
   h = 8
+  nil
 end
 
 Benchmark.ips do |x|
