53894bf4e7c91af36a27a65cc0ae2decd368a743
Juanito Fatas <katehuang0320@gmail.com>
2015-08-07 09:51:22 +0800

    Fix assignment method name

--- code/general/assignment.rb
+++ code/general/assignment.rb
@@ -1,11 +1,11 @@
 require 'benchmark/ips'
 
-def slow
+def fast
   a, b, c, d, e, f, g, h = 1, 2, 3, 4, 5, 6, 7, 8
   nil
 end
 
-def fast
+def slow
   a = 1
   b = 2
   c = 3
@@ -18,7 +18,7 @@ def fast
 end
 
 Benchmark.ips do |x|
-  x.report('Parallel Assignment')   { slow }
-  x.report('Sequential Assignment') { fast }
+  x.report('Parallel Assignment')   { fast }
+  x.report('Sequential Assignment') { slow }
   x.compare!
 end
