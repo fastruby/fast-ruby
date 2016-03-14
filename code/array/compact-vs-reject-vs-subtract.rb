0f4820ea6f487eed9a92ae09cd572aafdc2e9cbe
Gabriel Balbuena <gabrielbalbuena@gmail.com>
2016-03-14 18:51:50 -0300

    Allways dup, include conpact!

--- code/array/compact-vs-reject-vs-subtract.rb
+++ code/array/compact-vs-reject-vs-subtract.rb
@@ -1,21 +1,30 @@
 require 'benchmark/ips'
 
 ARRAY = Array.new(1000){ rand(2).nonzero? }
+ARRAY1 = ARRAY.dup
+ARRAY2 = ARRAY.dup
+ARRAY3 = ARRAY.dup
+ARRAY4 = ARRAY.dup
 
 def compact
-  ARRAY.compact
+  ARRAY1.compact
+end
+
+def compact!
+  ARRAY2.compact
 end
 
 def reject
-  ARRAY.reject(&:nil?)
+  ARRAY3.reject(&:nil?)
 end
 
 def subtract
-  ARRAY - [nil]
+  ARRAY4 - [nil]
 end
 
 Benchmark.ips do |x|
   x.report('A.compact') { compact }
+  x.report('A.compact!') { compact! }
   x.report('A.reject(&:nil?)') { reject }
   x.report('A - [nil]') { subtract }
   x.compare!
