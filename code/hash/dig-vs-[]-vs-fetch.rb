a759a251a4bf9022094db992c57dab1f827309c2
Dennis Ideler <ideler.dennis@gmail.com>
2016-03-18 00:13:48 +0000

    Add benchmark for Hash#dig vs #[] vs #fetch

---
+++ code/hash/dig-vs-[]-vs-fetch.rb
@@ -0,0 +1,27 @@
+require 'benchmark/ips'
+
+h = { a: { b: { c: { d: { e: "foo" } } } } }
+
+Benchmark.ips do |x|
+  x.report 'Hash#dig' do
+    h.dig(:a, :b, :c, :d, :e)
+  end
+
+  x.report 'Hash#[]' do
+    h[:a][:b][:c][:d][:e]
+  end
+
+  x.report 'Hash#[] fallback' do
+    ((((h[:a] || {})[:b] || {})[:c] || {})[:d] || {})[:e]
+  end
+
+  x.report 'Hash#fetch' do
+    h.fetch(:a).fetch(:b).fetch(:c).fetch(:d).fetch(:e)
+  end
+
+  x.report 'Hash#fetch fallback' do
+    h.fetch(:a, {}).fetch(:b, {}).fetch(:c, {}).fetch(:d, {}).fetch(:e, nil)
+  end
+
+  x.compare!
+end
