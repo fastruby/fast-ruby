f286da4963bdcf5346eefb60323e5e8462f61990
Bernie Chiu <bernie_chiu@hotmail.com>
2018-02-09 00:20:11 +0800

    Add another inject like method that might occur in ruby

--- code/enumerable/inject-symbol-vs-block.rb
+++ code/enumerable/inject-symbol-vs-block.rb
@@ -8,17 +8,24 @@ def fastest
 end
 
 def fast
-  ARRAY.inject(&:+)
+  a = 0
+  ARRAY.each { |i| a += i }
+  a
 end
 
 def slow
+  ARRAY.inject(&:+)
+end
+
+def slowest
   ARRAY.inject { |a, i| a + i }
 end
 
 Benchmark.ips do |x|
-  x.report('inject symbol') { fastest }
-  x.report('inject to_proc') { fast }
-  x.report('inject block')   { slow }
+  x.report('inject symbol')             { fastest }
+  x.report('each block without inject') { fast }
+  x.report('inject to_proc')            { slow }
+  x.report('inject block')              { slowest }
 
   x.compare!
 end
