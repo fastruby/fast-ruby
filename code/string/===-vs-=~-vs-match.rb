9ce5e978c4e2e2a06ade03c10d26cc57b07a20b7
Daniel Arnold <darnold8@gmail.com>
2018-09-06 15:02:48 -0400

    add Regexp#match? and Regexp#match to benchmark

--- code/string/===-vs-=~-vs-match.rb
+++ code/string/===-vs-=~-vs-match.rb
@@ -1,5 +1,9 @@
 require "benchmark/ips"
 
+def fastest
+  /boo/.match?('foo'.freeze)
+end
+
 def fast
   "foo".freeze.match?(/boo/)
 end
@@ -12,14 +16,20 @@ def slower
   /boo/ === "foo".freeze  
 end
 
+def even_slower
+  /boo/.match('foo'.freeze)
+end
+
 def slowest
   "foo".freeze.match(/boo/)
 end
 
 Benchmark.ips do |x|
+  x.report("Regexp#match?") { fastest } if RUBY_VERSION >= "2.4.0".freeze
   x.report("String#match?") { fast } if RUBY_VERSION >= "2.4.0".freeze
   x.report("String#=~") { slow }
   x.report("Regexp#===") { slower }
+  x.report("Regexp#match") { even_slower }
   x.report("String#match") { slowest }
   x.compare!
 end
