e1acd20f7f4ae952afb4f16ee9aa6ae52b3943e2
ParadoxV5 <paradox.ver5@gmail.com>
2020-06-21 13:36:08 -0600

    Update rational-vs-divide-with-r.rb
    String#to_r is uno más way

--- code/general/rational-vs-divide-with-r.rb
+++ code/general/rational-vs-divide-with-r.rb
@@ -1,26 +1,30 @@
 def avg
   Rational(2, 3)
 end
-def slow1
+def slowest1
   Rational('2/3')
 end
+def slow
+  '2/3'.to_r
+end
 
-def slow2
+def slowest2
   2/3r
 end
-def fast1
+def fastest1
   2r/3
 end
-def fast2
+def fastest2
   2r/3r
 end
 
 require 'benchmark/ips'
 Benchmark.ips do |bm|
   bm.report('Rational(2, 3)') {avg}
-  bm.report(%q"Rational('2/3')") {slow1}
-  bm.report('2/3r') {slow2}
-  bm.report('2r/3') {fast1}
-  bm.report('2r/3r') {fast2}
+  bm.report("Rational('2/3')") {slowest1}
+  bm.report("'2/3'.to_r") {slow}
+  bm.report('2/3r') {slowest2}
+  bm.report('2r/3') {fastest1}
+  bm.report('2r/3r') {fastest2}
   bm.compare!
 end
